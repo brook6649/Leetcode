8725ecfe229042d96ca76faca7b985bac134503b
Steven Jiang <39409877+brook6649@users.noreply.github.com>
2020-03-19 13:26:49 +0800

    Update Add_Two_Numbers.java

--- Linked List/Add_Two_Numbers.java
+++ Linked List/Add_Two_Numbers.java
@@ -1,3 +1,18 @@
+// Problem Description
+/* You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order and each of their nodes contain a single digit. 
+Add the two numbers and return it as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+Example:
+
+Input: (2 -> 4 -> 3) + (5 -> 6 -> 4)
+Output: 7 -> 0 -> 8
+Explanation: 342 + 465 = 807.
+*/
+
+
 /* public class ListNode {
 *    int val;
 *    ListNode next;
@@ -8,6 +23,9 @@
 */
 
 class Add_Two_Numbers {
+/** 从个位数(链表的头部节点)开始，每位做加法，把加和后这一位的结果(整数)保存在新建的节点内，carry [0 | 1] 为进位项.
+    循环体后多了条判断，为了应对诸如 1 + 99 = 100 这种位数增加的情况.
+*/
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode initList = new ListNode(0);
         ListNode cursor = initList;
